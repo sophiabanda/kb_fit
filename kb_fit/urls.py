ec7c679f557ca0dc0ee181ef98fda15857142b53
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-04-28 15:07:36 -0400

    list WIP

--- kb_fit/urls.py
+++ kb_fit/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('library/', views.library, name='library' ),
+    path('library/', views.ExerciseList.as_view(), name='library'),
+
 ]
